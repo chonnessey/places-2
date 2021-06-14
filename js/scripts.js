4b9f9c65ec0c2d40f5b0b774f55e890dc0c71109
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-14 11:10:24 -0700

    add new place to js language
    co-authored-by: Braden Nelson <connornel123@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,8 @@ let portland = new Place("Portland", ["Burnside Bridge", "Moda Center"], "Fall")
 let austin = new Place("Austin", ["Texas capitol building", "Mckinney Falls Parlk"]
 , "winter")
 
+let mexico = new Place("Sinaloa", ["Mazatlan", "Guamuchil"], "Summer")
+
 
 
 
