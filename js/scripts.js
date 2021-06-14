571f68f36aec5c4a0d465f7251c5a2b88725a89c
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-14 14:09:07 -0700

    add UI logic to page
    co-authored-by: Braden Nelson <connornel123@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business logic for places
+// Business logic
 
 function Place(location, landmarks, year) {
   this.location = location;
@@ -15,5 +15,32 @@ let austin = new Place("Austin", ["Texas capitol building", "Mckinney Falls Parl
 let mexico = new Place("Sinaloa", ["Mazatlan", "Guamuchil"], "Summer")
 
 
+// UI logic
 
+$(document).ready(function() {
+  $("#mexico").click(function() {
+  const mexicoKeys = Object.keys(mexico);
+  let mexicoString = "";
+  mexicoKeys.forEach(function(key) {
+    mexicoString = mexicoString.concat(key + ":" + mexico [key] + "\n" );
+  });
+  $(".page-mexico").append("<li>" + mexicoString + "</li>");
+  })
 
+  $("#portland").click(function() {
+  const portlandKeys = Object.keys(portland);
+  let portlandString = "";
+  portlandKeys.forEach(function(key) {
+    portlandString = portlandString.concat(key + ":" + portland [key] + "\n" );
+  });
+  $(".page-portland").append("<li>" + portlandString + "</li>");
+  })
+  $("#austin").click(function() {
+  const austinKeys = Object.keys(austin);
+  let austinString = "";
+  austinKeys.forEach(function(key) {
+    austinString = austinString.concat(key + ":" + austin [key] + "\n" );
+  });
+  $(".page-austin").append("<li>" + austinString + "</li>");
+  })
+})
