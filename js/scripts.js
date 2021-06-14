fc0fbc5f7ce667fe8704169529692ba22fbdabae
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-14 11:06:39 -0700

    add initial HTML and js language
    co-authored-by: Braden Nelson <connornel123@outlook.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Business logic for places
+
+function Place(location, landmarks, year) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.year = year;
+}
+
+
+let portland = new Place("Portland", ["Burnside Bridge", "Moda Center"], "Fall")
+
+let austin = new Place("Austin", ["Texas capitol building", "Mckinney Falls Parlk"]
+, "winter")
+
+
+
+
